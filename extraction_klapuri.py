0b17c519a55f0808d4f237a8401fd2ed85c7a67e
Marc <marc.siquier@bsc.es>
2017-02-04 19:52:59 +0100

    Saarland extracted with klapuri

--- extraction_klapuri.py
+++ extraction_klapuri.py
@@ -8,23 +8,42 @@ from essentia import pool
 import numpy as np
 import matplotlib.pyplot as plt
 
-inp = '/home/manu/Dropbox/MasterUPF/Asignaturas/2Trim/Music Information Retrieval/MIR Project/evaluation/Dataset/Saarland/Bach_BWV849-01_001_20090916-SMD.mp3'
+def fetchFiles(inputDir, descExt=".json"):
+    files = []
+    for path, dname, fnames  in os.walk(inputDir):
+        for fname in fnames:
+            if descExt in fname.lower():
+                files.append((path, fname))
+    return files
 
-inpm = '/home/manu/Dropbox/MasterUPF/Asignaturas/2Trim/Music Information Retrieval/MIR Project/evaluation/Dataset/Saarland/Bach_BWV849-01_001_20090916-SMD.mid'
+def klapuri_extractor(inp, name):
+	loader = essentia.standard.MonoLoader(filename = inp)
+	audio = loader()
+	multip = MultiPitchKlapuri(frameSize=2048, hopSize=441)
+	f0vec = multip(audio)
+	time = 0
+	f0_names = 'evaluation/Saarland_klapuri/' + name + '.f0s'
+	print 'Writing results to: ' + f0_names
 
-loader = essentia.standard.MonoLoader(filename = inp)
-audio = loader()
-multip = MultiPitchKlapuri(frameSize=2048, hopSize=441)
-f0vec = multip(audio)
-time = 0
+	f = open(f0_names, 'w')
+	for f0 in f0vec:
+		f.write(str(time) + "\t")
+		for f0s in f0:
+			f.write(str(f0s) + "\t")
+		f.write("\n")
+		time += 0.01
 
-os.chdir('..')
-os.chdir('evaluation')
 
-f = open('dataKlapuri.txt', 'w')
-for f0 in f0vec:
-	f.write(str(time) + "\t")
-	for f0s in f0:
-		f.write(str(f0s) + "\t")
-	f.write("\n")
-	time += 0.01
+
+def main():
+    filenames = fetchFiles('/home/marcsiq/SMC/MIRProject/Dataset/Saarland', '.mp3')
+
+    for path, fname in filenames:
+    	print "Extracting " + fname
+        file_location = path + "/" + fname
+        file_name, extension = os.path.splitext(fname)
+        klapuri_extractor(file_location, file_name)
+  
+
+if __name__ == "__main__":
+    main()
